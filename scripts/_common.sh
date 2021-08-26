8939d35f672e603011e7e09039f7d5ac29b6b4a4
yalh76 <yalh@yahoo.com>
2021-08-26 21:14:07 +0200

    Fix package_check

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,6 +9,8 @@ pkg_dependencies="xorg xinit dbus-x11 kodi"
 
 arm_pkg_dependencies="xserver-xorg-legacy xorg dbus-x11 kodi"
 
+package_check_dependencies="xserver-xorg-video-dummy"
+
 #=================================================
 # PERSONAL HELPERS
 #=================================================
