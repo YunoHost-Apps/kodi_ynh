d65ef911dee0f227dcd230712d26482b704b44a2
ericgaspar <junk.eg@free.fr>
2021-11-12 09:07:35 +0100

    cleaning up

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -19,6 +19,30 @@ package_check_dependencies="xserver-xorg-video-dummy"
 # EXPERIMENTAL HELPERS
 #=================================================
 
+# Check the architecture
+#
+# example: architecture=$(ynh_detect_arch)
+#
+# usage: ynh_detect_arch
+#
+# Requires YunoHost version 2.2.4 or higher.
+
+ynh_detect_arch(){
+	local architecture
+	if [ -n "$(uname -m | grep arm64)" ] || [ -n "$(uname -m | grep aarch64)" ]; then
+		architecture="arm64"
+	elif [ -n "$(uname -m | grep 64)" ]; then
+		architecture="x86-64"
+	elif [ -n "$(uname -m | grep 86)" ]; then
+		architecture="i386"
+	elif [ -n "$(uname -m | grep arm)" ]; then
+		architecture="arm"
+	else
+		architecture="unknown"
+	fi
+	echo $architecture
+}
+
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
